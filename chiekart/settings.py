52183f6a45c274c784c10b5cb66da119d662cfbd
chie <chiecruzcayanan@gmail.com>
2025-08-27 21:58:40 +0800

    db url

--- chiekart/settings.py
+++ chiekart/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-chiestore-env.eba-ssepaqb2.us-west-2.elasticbeanstalk.com', "*"]
+ALLOWED_HOSTS = ['django-chiestore-env.eba-ssepaqb2.us-west-2.elasticbeanstalk.com', "awseb-e-bcdih7wzzy-stack-awsebrdsdatabase-avuqhnyczccq.cfgaawsqm9ae.us-west-2.rds.amazonaws.com:5432"]
 
 
 # Application definition
